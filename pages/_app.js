e79e55fa278581a68da1a5d10f8103e473827b84
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-04-11 19:26:18 -0300

    Share URL Image

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,9 @@ function MyApp({ Component, pageProps }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />   
         <meta property="og:title" content="Gandini Music"/>
+        <meta property="og:type" content="video.movie" />
         <meta property="og:url" content="https://www.gandinimusic.com/"/>
-        <meta property="og:image" content="https://www.gandinimusic.com/Images/BackgroundLeft.jpg"/>
+        <meta property="og:image" content="https://www.gandinimusic.com/Images/share.jpg"/>
         <meta property="og:description" content="Gandini Music Web Landing Page"/>
       </Head>
       <Component {...pageProps} />
